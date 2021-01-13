7cfd6b762998344a673be723f0f20a2247bbc576
vagrant <vagrant@vagrant.vm>
2021-01-13 01:35:29 +0000

    finsihed lab exercise

--- markov.py
+++ markov.py
@@ -50,11 +50,14 @@ def make_chains(text_string):
     words = text_string.split()
     # print(line)
 
+# Would, you, could, you, in, a house?, Would, you could you with a mouse?
+
     for i in range(len(words) - 2):
-        pairs = (words[i], words[i + 1])
+        pairs = (words[i], words[i + 1]) # [0], [1]
+        
         if pairs in chains:
         
-            chains[pairs].append(words[i + 2])
+            chains[pairs].append(words[i + 2]) #[2]
             #chains.get(pairs)
         else:
             chains[pairs] = [words[i + 2]]
@@ -79,15 +82,53 @@ def make_text(chains):
     #print(chains)
     
     #chains[keys]= values
-    random_text = choice(list(chains.keys()))
- 
-    print(random_text)
-    
+    random_text_key = choice(list(chains.keys()))
+    # print(f'this is random text key: {random_text_key}')
+    random_text_value = choice(chains[random_text_key])
 
+    words.extend([random_text_key[0], random_text_key[1], random_text_value])
+    #[('would', 'you')] => ['would', 'you']
+    """
+    1. Make a new key out of the second word in the first key and the 
+        random word you pulled out from the list of words that followed it.
+    2. Look up that new key in the dictionary, and pull a new random word 
+        out of the new list.
+    3. Keep doing that until your program raises a KeyError.
+
+    We used choice() to get (and, ham?) to start our "story"
+    Used choice() to pick what comes next based on (and, ham?)
+
+    words = [and, ham?, Would]
+    Go use (ham?, Would) to find the next word by doing choice()
+    """
+
+    while True:
+
+        two_word_story = (words[-2], words[-1])
+        # ^ is (ham?, Would)
+      
+        # print(two_word_story)
+        # print(one_word_story)
+        
+        
+        # if one_word_story == "am?":
+        #     break
+        #     
+        if two_word_story in chains:
+            one_word_story = choice(chains[two_word_story])
+            words.extend([one_word_story])
+        else:
+            break
+        # elif two_word_story == choice(list(chains.keys())) and one_word_story = choice(chains[random_text_key]):                   
+        #     words.extend([two_word_story[0], random_text_key[1], random_text_value])
+
+    
+   
     return ' '.join(words)
 
 
-input_path = 'green-eggs.txt'
+
+input_path = 'gettysburg.txt'
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
